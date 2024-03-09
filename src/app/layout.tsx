1e2864b6dd4ce5dd1b1961513955d3b20208fe10
Maciej Adamski <maciej.adamski@kyndryl.com>
2024-03-09 20:34:59 +0100

    ref: font imports and update component names

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,31 +5,19 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 // * import fonts
-import { Inter, Noto_Sans, Lobster, Sora } from "next/font/google";
+import { Sora } from "next/font/google";
 
 // * components
-import Nav from "@/components/navbar";
+import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
-const inter = Inter({ subsets: ["latin"] });
-
-const noto = Noto_Sans({
-    subsets: ["latin"],
-    weight: ["400", "500", "600", "700"],
-    variable: "--font-noto",
-});
-
-const lobster = Lobster({
-    weight: ["400"],
-    subsets: ["latin"],
-    variable: "--font-lobster",
-});
-
+// Fill metadata if you want to use as webpage.Here For showcase is not necessary.
 export const metadata: Metadata = {
     title: "Famsec",
-    description: "",
+    description: "Insurance for your family",
 };
 
+// * font setup
 const sora = Sora({
     subsets: ["latin"],
     weight: ["400", "500", "600", "700"],
@@ -40,7 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html className="h-full" lang="en">
             <body className={`flex flex-col min-h-full ${sora.className}`}>
-                <Nav />
+                <Navbar />
                 {children}
                 <Footer />
             </body>
